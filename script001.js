15600e16fa7f5c1d9f88804af99169b425a9ac5a
Divine Orji <dpkreativ@gmail.com>
2022-07-15 06:29:26 +0100

    Find the area of a triangle given its three sides

--- script001.js
+++ script001.js
@@ -3,8 +3,9 @@
  */
 
 function AreaOfTriangle(a, b, c) {
-  let s = (a + b + c) / 2;
-  return Math.ceil(Math.sqrt(s * (s - a) * (s - b) * (s - c)) * 10) / 10;
+  const s = (a + b + c) / 2;
+  const area = Math.ceil(Math.sqrt(s * (s - a) * (s - b) * (s - c)) * 10) / 10;
+  return area;
 }
 
 console.log(AreaOfTriangle(5, 6, 7));
