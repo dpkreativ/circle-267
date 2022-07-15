955762d56602dd209685582b99385ae8cff4ec44
Divine Orji <dpkreativ@gmail.com>
2022-07-15 06:31:45 +0100

    Accept two integers and display the larger integer

--- script003.js
+++ script003.js
@@ -1,3 +1,9 @@
 /**
  * Write a JavaScript program that accept two integers and display the larger
  */
+
+function largerInteger(a, b) {
+  return a > b ? a : b;
+}
+
+console.log(largerInteger(7, 12));
