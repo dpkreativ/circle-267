d84bda906819a7122324d93a9e2c265afd067912
Divine Orji <dpkreativ@gmail.com>
2022-07-15 06:23:45 +0100

    Check positive and negative integers

---
+++ script002.js
@@ -0,0 +1,9 @@
+/*
+Write a JavaScript program to check from two given integers, whether one is positive and another one is negative
+*/
+
+function checkIntegers(a, b) {
+  return a > 0 ? b < 0 : b > 0;
+}
+
+console.log(checkIntegers(2, -3));
