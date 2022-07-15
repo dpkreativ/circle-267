0d46ae832dae85aeaf81c0ba91293487dde15695
Divine Orji <dpkreativ@gmail.com>
2022-07-15 06:35:45 +0100

    Loop from 0 to 15, show if a number is odd or even

---
+++ script004.js
@@ -0,0 +1,7 @@
+/**
+ * Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+ */
+
+for (let i = 0; i <= 15; i++) {
+  console.log(i % 2 === 0 ? `${i} is even` : `${i} is odd`);
+}
